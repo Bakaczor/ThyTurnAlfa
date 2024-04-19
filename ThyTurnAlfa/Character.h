5611100bc90cdc979813bab2123e3b47d967d28f
Maksymilian Czapski <maks.czapski@gmail.com>
2024-04-19 20:05:27 +0200

    character

--- ThyTurnAlfa/Character.h
+++ ThyTurnAlfa/Character.h
@@ -1,10 +1,20 @@
 #pragma once
+#include <iostream>
+#include <json/json.h>
 
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
 class Character {
-
+public:
+	void Deserialize(Json::Value& root);
+	std::string getName() {
+		return m_name;
+	}
+private:
+	std::string m_name;
+	int m_SPD = -1;
+	bool m_isAlive = true;
 };
 
 #endif
