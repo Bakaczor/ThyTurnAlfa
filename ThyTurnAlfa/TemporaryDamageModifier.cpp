b744e2c195b55733e6ed9095bea7b150740d527d
xNetru <kamilbbank@gmail.com>
2024-04-20 03:42:09 +0200

    Restoring previous body of nextRound method in TemporaryDamageModifier

--- ThyTurnAlfa/TemporaryDamageModifier.cpp
+++ ThyTurnAlfa/TemporaryDamageModifier.cpp
@@ -4,27 +4,8 @@ bool TemporaryDamageModifier::nextRound(Character& affected)
 {
 	if (elapsedRounds == 0)
 	{
-		int modified_w_atk = affected.wAtk + atkModifier;
-		if (modified_w_atk < MIN_ATK_VALUE)
-		{
-			atkModifier = MIN_ATK_VALUE - affected.wAtk;
-			affected.wAtk = MIN_ATK_VALUE;
-		}
-		else
-		{
-			affected.wAtk = modified_w_atk;
-		}
-		
-		int modified_w_def = affected.wDef + defModifier;
-		if (modified_w_def < MIN_DEF_VALUE)
-		{
-			defModifier = MIN_DEF_VALUE - affected.wDef;
-			affected.wDef = 0;
-		}
-		else
-		{
-			affected.wDef = modified_w_def;
-		}
+		affected.wAtk += atkModifier;
+		affected.wDef += defModifier;
 
 	}
 	if (elapsedRounds == duration)
