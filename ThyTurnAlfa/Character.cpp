15ecd6edb1661f83c2d804383e5dc7cdfef31a33
xNetru <kamilbbank@gmail.com>
2024-04-23 23:54:25 +0200

    fixed applyEffects in Character

--- ThyTurnAlfa/Character.cpp
+++ ThyTurnAlfa/Character.cpp
@@ -2,21 +2,14 @@
 #include "Effect.hpp"
 #include "Movement.hpp"
 
-bool Character::addEffect(Effect& c) {
-	return false;
-}
-
 bool Character::applyEffects() {
 	for (auto it = activeEffects.begin(); it != activeEffects.end(); it++)
 	{
 		if (!(*it)->nextRound(*this))
+		{
+			it->get()->cancelFrom(*this);
 			activeEffects.erase(it);
+		}
 	}
 	return true;
 }
-
-//void Character::Deserialize(Json::Value& root)
-//{
-//	m_name = root["name"].asString();
-//	m_SPD = root["SPD"].asInt();
-//}
