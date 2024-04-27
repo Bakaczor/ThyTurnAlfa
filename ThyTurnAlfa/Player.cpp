924d9cd5fb04be4bac7f8c97d05c3d4810193c6c
Bakaczor <bartosz.kaczorowski@protonmail.com>
2024-04-27 16:30:55 +0200

    fix bad subscript

--- ThyTurnAlfa/Player.cpp
+++ ThyTurnAlfa/Player.cpp
@@ -4,6 +4,7 @@ unsigned int Player::m_count = 0;
 
 Player::Player(std::vector<Character>& availibleCharacters, std::array<int, 4>& curChrIds): id{ m_count++ } {
 	for (const int& i : curChrIds) {
+		if (i == -1) { continue; }
 		party.push_back(&availibleCharacters[i]);
 	}
 }
