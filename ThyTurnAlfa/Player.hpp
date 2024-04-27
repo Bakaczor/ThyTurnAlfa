ddef45363bfd173e7cad320e510e1878f793f295
Bakaczor <bartosz.kaczorowski@protonmail.com>
2024-04-27 15:36:03 +0200

    solved conflicts

--- ThyTurnAlfa/Player.hpp
+++ ThyTurnAlfa/Player.hpp
@@ -6,17 +6,16 @@
 #include "Character.hpp"
 
 class Player {
-public:
+	public:
 	unsigned int id;
 	std::vector<Character*> party;
 
 	Player(std::vector<Character>& availibleCharacters, std::array<int, 4>& curChrIds);
-	bool virtual move(Character& who) = 0;
-
 	Player() = default;
-	Player(std::vector<Character>& party);
-	virtual bool move(Character& character, std::array<Player, 2>& players);
-private:
+
+	virtual bool move(Character& character, std::array<Player, 2>& players) = 0;
+
+	private:
 	static unsigned int m_count;
 };
 
