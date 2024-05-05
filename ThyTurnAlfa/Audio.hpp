3b36207c64b3bd79495f8f3380343064dc5606e8
Bakaczor <bartosz.kaczorowski@protonmail.com>
2024-05-05 23:04:02 +0200

    small adjustments

--- ThyTurnAlfa/Audio.hpp
+++ ThyTurnAlfa/Audio.hpp
@@ -3,30 +3,30 @@
 #ifndef AUDIO_HPP
 #define AUDIO_HPP
 
-#include <unordered_map>
-#include <tuple>
 #include <memory>
+#include <tuple>
+#include <unordered_map>
 
 #include <SFML/Audio.hpp>
 
 #include "MusicState.hpp"
 
 class Audio {
-public:
+	public:
 	Audio(const Audio&) = delete;
 	
 	static Audio& get();
 
 	bool playMusic(MusicState state);
 
-private:
-	Audio();
-	~Audio();
-
+	private:
 	static Audio m_audio;
 
 	std::unordered_map<MusicState, std::tuple<std::unique_ptr<sf::Music>, bool>> m_audioPlayers;
 	MusicState m_recentlyPlayedMusic = MusicState::None;
+
+	Audio();
+	~Audio();
 };
 
 #endif // !AUDIO_HPP
